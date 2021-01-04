bf365d1493573c321706a8561876b79ce0dc3666
Paul Livesey <paullivesey@Pauls-MacBook-Pro.local>
2021-01-04 11:20:29 +0800

    First additions with 4 (VERY) basic strategies.

---
+++ run_strategy.py
@@ -0,0 +1,67 @@
+import os, sys, argparse, datetime
+import pandas as pd
+import backtrader as bt
+from backtrader import Cerebro
+from strategies.GoldenCross import GoldenCross
+from strategies.BuyHold import BuyHold
+from strategies.strategy import TestStrategy
+from strategies.rsimean import RSIMean
+from strategies.bbands import BBands
+
+if __name__ == '__main__':
+    cerebro = bt.Cerebro()
+
+    # prices = pd.read_csv('data/spy_2000-2020.csv', index_col='Date', parse_dates=True)
+
+    # initialize the Cerebro engine
+    cerebro = Cerebro()
+    START_VALUE = 1000
+    cerebro.broker.setcash(START_VALUE)
+
+    # add OHLC data feed
+    # feed = bt.feeds.PandasData(dataname=prices)
+    feed = bt.feeds.YahooFinanceData(
+        dataname='TTSH',
+        # Do not pass values before this date
+        fromdate=datetime.datetime(2019, 1, 1),
+        # Do not pass values after this date
+        todate=datetime.datetime(2019, 6, 30),
+        reverse=False)
+
+    cerebro.adddata(feed)
+
+    strategies = {
+        "golden_cross": GoldenCross,
+        "buy_hold": BuyHold,
+        "strategy": TestStrategy,
+        "rsi_mean": RSIMean,
+        "bbands": BBands
+    }
+
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("strategy", help="Which strategy to run", type=str)
+    args = parser.parse_args()
+
+    if not args.strategy in strategies:
+        print("Invalid strategy, must select one of {}".format(strategies.keys()))
+        sys.exit()
+
+    cerebro.addstrategy(strategy=strategies[args.strategy])
+
+    # Add a FixedSize sizer according to the stake
+    cerebro.addsizer(bt.sizers.AllInSizer)
+
+
+    cerebro.broker.setcommission(commission=0.000)  # 0.5% of the operation value
+
+    cerebro.run()
+
+    # cerebro.strategy.set_sma(opt_slow, opt_fast)
+
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    print('Final Cash Value: %.2f' % cerebro.broker.get_cash())
+    roi = (cerebro.broker.get_value() / START_VALUE) - 1.0
+    print('ROI:        {:.2f}%'.format(100.0 * roi))
+
+    # cerebro.plot()
